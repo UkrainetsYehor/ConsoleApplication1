15becf46deebb180eac0df4e093c26bae96ebd8f
Ehor Ukrainets <0407aezakmi@gmail.com>
2023-04-29 15:09:22 +0300

    Fakecoin3

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,167 @@
-﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <stdio.h>
+#include <stdint.h>
 
-#include <iostream>
 
-int main()
+
+short findFakeCoin(short coin1, short coin2, short coin3, short coin4,
+    short coin5, short coin6, short coin7, short coin8)
 {
-    std::cout << "Hello World!";
+    short originCoin = 1;
+    short fakeCoin = 0;
+
+    if (coin1 > originCoin)
+    {
+        fakeCoin = 1;
+    }
+
+    if (coin2 > originCoin)
+    {
+        fakeCoin = 2;
+    }
+
+    if (coin3 > originCoin)
+    {
+        fakeCoin = 3;
+    }
+
+    if (coin4 > originCoin)
+    {
+        fakeCoin = 4;
+    }
+
+    if (coin5 > originCoin)
+    {
+        fakeCoin = 5;
+    }
+
+    if (coin6 > originCoin)
+    {
+        fakeCoin = 6;
+    }
+
+    if (coin7 > originCoin)
+    {
+        fakeCoin = 7;
+    }
+
+    if (coin8 > originCoin)
+    {
+        fakeCoin = 8;
+    }
+
+    return fakeCoin;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню 
+short findFakeCoin2(short cn[8])
+{
+    short originCoin = 1;
+    short fakeCoin = 0;
+
+    for (int num = 0; num < 8; num++)
+    {
+        if (cn[num] > originCoin)
+        {
+            fakeCoin = num;
+        }
+    }
+
+    fakeCoin = fakeCoin + 1;
+
+    return fakeCoin;
+}
+
+short findFakecoin3(short cn[8])
+{
+    //  1 2 3  4 5 6  7 8
+    //  1 ta 2
+    short fakeCoin = 8;
+    if (cn[0] > cn[1])
+    {
+        fakeCoin =1 ;
+    }
+    else if (cn[1] > cn[2])
+    {
+        fakeCoin =2 ;
+    }
+    else if (cn[2] > cn[3])
+    {
+        fakeCoin =3 ;
+    }
+    else if (cn[3] > cn[4])
+    {
+        fakeCoin =4 ;
+    }
+    else if (cn[4] > cn[5])
+    {
+        fakeCoin =5 ;
+    }
+    else if (cn[5] > cn[6])
+    {
+        fakeCoin =6 ;
+    }
+    else if (cn[6] > cn[7])
+    {
+        fakeCoin =7 ;
+    }
+    else
+    { 
+        fakeCoin = 8; 
+    }
+    
+
+
+    return fakeCoin;
+}
+
+
+ int main()
+{
+    const short origin = 1;
+
+    short coin[8] = { 0,0,0,0,0,0,0,0 };
+
+    coin[0] = 1;
+    coin[1] = 1;
+    coin[2] = 1;
+    coin[3] = 1;
+    coin[4] = 1;
+    coin[5] = 1;
+    coin[6] = 3;
+    coin[7] = 1;
+
+    short fakeCoin;
+
+
+    fakeCoin = findFakeCoin(coin[0], coin[1], coin[2], coin[3], coin[4], coin[5], coin[6], coin[7]);
+    printf("coin %d is fake!\n", fakeCoin);
+
+    coin[0] = 1;
+    coin[1] = 2;
+    coin[2] = 1;
+    coin[3] = 1;
+    coin[4] = 1;
+    coin[5] = 1;
+    coin[6] = 1;
+    coin[7] = 1;
+
+    fakeCoin = findFakeCoin(coin[0], coin[1], coin[2], coin[3], coin[4], coin[5], coin[6], coin[7]);
+    printf("coin %d is fake!\n", fakeCoin);
+
+    coin[0] = 1;
+    coin[1] = 1;
+    coin[2] = 1;
+    coin[3] = 1;
+    coin[4] = 1;
+    coin[5] = 5;
+    coin[6] = 1;
+    coin[7] = 1;
+     fakeCoin = findFakeCoin2(coin);
+     printf("coin %d is fake!\n", fakeCoin);
+
+     fakeCoin = findFakecoin3(coin);
+     printf("coin %d is fake!\n", fakeCoin);
+
+
+
+}
